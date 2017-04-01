fd42f5f800bbbc96e09d379283ff0f01416d196c
Jamie Czuy <jamie.czuy@gmail.com>
2017-04-01 09:20:46 -0400

    Add URL for SoloDetailView

--- jmad-project/jmad/jmad/urls.py
+++ jmad-project/jmad/jmad/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls import url
 from django.contrib import admin
 
 import solos.views as solos_views
+from solos.views import SoloDetailView
 
 
 urlpatterns = [
     url(r'^$', solos_views.index),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^solos/(?P<pk>\d+)/$', SoloDetailView.as_view()),
 ]
