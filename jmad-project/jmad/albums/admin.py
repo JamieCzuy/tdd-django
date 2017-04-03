7946a426d0884e39de8dd5a6a7fb18bc9da5784c
Jamie Czuy <jamie.czuy@gmail.com>
2017-04-02 23:00:16 -0400

    Adds str, ordering, and columns for Track admin

--- jmad-project/jmad/albums/admin.py
+++ jmad-project/jmad/albums/admin.py
@@ -4,5 +4,9 @@ from .models import Album
 from .models import Track
 
 
+class TrackAdmin(admin.ModelAdmin):
+    model = Track
+    list_display = ('album', 'name', 'track_number')
+
 admin.site.register(Album)
-admin.site.register(Track)
+admin.site.register(Track, TrackAdmin)
--- jmad-project/jmad/jmad/tests.py
+++ jmad-project/jmad/jmad/tests.py
@@ -91,12 +91,14 @@ class StudentTestCase(LiveServerTestCase):
 
         self.track4 = Track.objects.create(
             name='Freddie Freeloader',
-            album=self.album2
+            album=self.album2,
+            track_number=2
         )
 
         self.track5 = Track.objects.create(
             name='Blue in Green',
-            album=self.album2
+            album=self.album2,
+            track_number=3
         )
 
     def tearDown(self):
@@ -256,6 +258,15 @@ class StudentTestCase(LiveServerTestCase):
         # link and sees the Tracks that have been added.
         # They're ordered first by Album, then by track
         # number.
+        self.browser.find_element_by_css_selector('#site-name a').click()
+        self.browser.find_element_by_link_text('Tracks').click()
+        track_rows = self.browser.find_elements_by_css_selector('#result_list tr')
+
+        self.assertEqual(track_rows[1].text, 'Kind of Blue Freddie Freeloader 2')
+        self.assertEqual(track_rows[2].text, 'Kind of Blue Blue in Green 3')
+        self.assertEqual(track_rows[3].text, 'Kind of Blue All Blues 4')
+        self.assertEqual(track_rows[4].text, 'Know What I Mean? Waltz for Debby -')
+        self.assertEqual(track_rows[5].text, 'My Favorite Things My Favorite Things -')
 
         # He adds a track to an album that already exists
 
